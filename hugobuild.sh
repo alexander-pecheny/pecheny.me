44998be2dc49c4ca194b3c41dee5496f37036246
Alexander Pecheny <ap@pecheny.me>
2024-02-21 16:25:06 +0400

    fix build

--- hugobuild.sh
+++ hugobuild.sh
@@ -1,8 +1,10 @@
 set -e
 rm -r public
 hugo
+mv .tmpgit .tmpgit-prev
 cp -r ~/peczony.github.io/.git .tmpgit
 rm -rf ~/peczony.github.io/
 mkdir ~/peczony.github.io
 cp -r public/* ~/peczony.github.io/
 cp -r .tmpgit ~/peczony.github.io/.git
+rm -rf .tmpgit-prev
