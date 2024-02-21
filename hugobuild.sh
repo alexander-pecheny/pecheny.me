2de989d47fafb4ff56e7003842f60025099b3575
Alexander Pecheny <ap@pecheny.me>
2024-02-21 16:21:29 +0400

    add missing dreams & fix bash script

--- hugobuild.sh
+++ hugobuild.sh
@@ -1,3 +1,8 @@
+set -e
 rm -r public
 hugo
+cp -r ~/peczony.github.io/.git .tmpgit
+rm -rf ~/peczony.github.io/
+mkdir ~/peczony.github.io
 cp -r public/* ~/peczony.github.io/
+cp -r .tmpgit ~/peczony.github.io/.git
